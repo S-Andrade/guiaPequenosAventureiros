5991ad160dfed36cd7acab42423a411134625866
brunorabacal <brunorabacal@ua.pt>
2020-05-15 19:01:51 +0100

    Tirar comments esquecidos dos testes

--- app_criancas/guia_pa_criancas/lib/models/companheiro.dart
+++ app_criancas/guia_pa_criancas/lib/models/companheiro.dart
@@ -1,35 +1,35 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Companheiro {
-//  String bodyPart;
-//  String color;
-//  String eyes;
-//  String headTop;
-//  String id;
+  String bodyPart;
+  String color;
+  String eyes;
+  String headTop;
+  String id;
   String shape;
+  DocumentReference owner;
 
   Companheiro();
 
   Companheiro.fromMap(Map<String, dynamic> data) {
-//    bodyPart = data['body_part'];
-//    color = data['color'];
-//    eyes = data['eyes'];
-//    headTop = data['head_top'];
-//    id = data['id'];
-//    shape = data['shape'];
-      shape = 'bolinha';
-//    owner = data['owner'];
+    bodyPart = data['body_part'];
+    color = data['color'];
+    eyes = data['eyes'];
+    headTop = data['head_top'];
+    id = data['id'];
+    shape = data['shape'];
+    owner = data['owner'];
   }
 
   Map<String, dynamic> toMap() {
     return {
-//      'body_part' : bodyPart,
-//      'color': color,
-//      'eyes' : eyes,
-//      'head_top' : headTop,
-//      'id' : id,
-        'shape': shape,
-//      'owner' : owner,
+      'body_part' : bodyPart,
+      'color': color,
+      'eyes' : eyes,
+      'head_top' : headTop,
+      'id' : id,
+      'shape': shape,
+      'owner' : owner,
     };
   }
 }
--- app_criancas/guia_pa_criancas/lib/services/companheiro_api.dart
+++ app_criancas/guia_pa_criancas/lib/services/companheiro_api.dart
@@ -18,62 +18,62 @@ getCompanheiro(String userEmail) async {
   return comp;
 }
 
-////CRIAR COMPANHEIRO
-//  createCompanheiro(String bodyPart, String color, String eyes, String headTop,
-//      String userEmail, String shape) async {
-//    CollectionReference companheiroRef =
-//        Firestore.instance.collection('companheiro');
-//    CollectionReference alunoRef = Firestore.instance.collection('aluno');
-//    DocumentReference companheiroDocRef = companheiroRef.document(userEmail);
-//    DocumentReference owner = alunoRef.document(userEmail);
-//    Companheiro companheiro = new Companheiro();
-//    companheiro.bodyPart = bodyPart;
-//    companheiro.color = color;
-//    companheiro.eyes = eyes;
-//    companheiro.headTop = headTop;
-//    companheiro.id = userEmail;
-//    companheiro.shape = shape;
-//    companheiro.owner = owner;
-//    await companheiroDocRef.setData(companheiro.toMap());
-//  }
-//
-////UPDATE COMPANHEIRO
-//  updateCompanheiroBody(String userEmail, String bodyPart) async {
-//    CollectionReference companheiroRef =
-//        Firestore.instance.collection('companheiro');
-//    DocumentReference companheiroDocRef = companheiroRef.document(userEmail);
-//    await companheiroDocRef.updateData({'body_part': bodyPart});
-//  }
-//
-////UPDATE COLOR
-//  updateCompanheiroColor(String userEmail, String color) async {
-//    CollectionReference companheiroRef =
-//        Firestore.instance.collection('companheiro');
-//    DocumentReference companheiroDocRef = companheiroRef.document(userEmail);
-//    await companheiroDocRef.updateData({'color': color});
-//  }
-//
-////UPDATE EYES
-//  updateCompanheiroEyes(String userEmail, String eyes) async {
-//    CollectionReference companheiroRef =
-//        Firestore.instance.collection('companheiro');
-//    DocumentReference companheiroDocRef = companheiroRef.document(userEmail);
-//    await companheiroDocRef.updateData({'eyes': eyes});
-//  }
-//
-////UPDATE HEAD TOP
-//  updateCompanheiroHead(String userEmail, String headTop) async {
-//    CollectionReference companheiroRef =
-//        Firestore.instance.collection('companheiro');
-//    DocumentReference companheiroDocRef = companheiroRef.document(userEmail);
-//    await companheiroDocRef.updateData({'head_top': headTop});
-//  }
-//
-////UPDATE SHAPE
-//  updateCompanheiroShape(String userEmail, String shape) async {
-//    CollectionReference companheiroRef =
-//        Firestore.instance.collection('companheiro');
-//    DocumentReference companheiroDocRef = companheiroRef.document(userEmail);
-//    await companheiroDocRef.updateData({'shape': shape});
-//  }
-//
+//CRIAR COMPANHEIRO
+  createCompanheiro(String bodyPart, String color, String eyes, String headTop,
+      String userEmail, String shape) async {
+    CollectionReference companheiroRef =
+        Firestore.instance.collection('companheiro');
+    CollectionReference alunoRef = Firestore.instance.collection('aluno');
+    DocumentReference companheiroDocRef = companheiroRef.document(userEmail);
+    DocumentReference owner = alunoRef.document(userEmail);
+    Companheiro companheiro = new Companheiro();
+    companheiro.bodyPart = bodyPart;
+    companheiro.color = color;
+    companheiro.eyes = eyes;
+    companheiro.headTop = headTop;
+    companheiro.id = userEmail;
+    companheiro.shape = shape;
+    companheiro.owner = owner;
+    await companheiroDocRef.setData(companheiro.toMap());
+  }
+
+//UPDATE COMPANHEIRO
+  updateCompanheiroBody(String userEmail, String bodyPart) async {
+    CollectionReference companheiroRef =
+        Firestore.instance.collection('companheiro');
+    DocumentReference companheiroDocRef = companheiroRef.document(userEmail);
+    await companheiroDocRef.updateData({'body_part': bodyPart});
+  }
+
+//UPDATE COLOR
+  updateCompanheiroColor(String userEmail, String color) async {
+    CollectionReference companheiroRef =
+        Firestore.instance.collection('companheiro');
+    DocumentReference companheiroDocRef = companheiroRef.document(userEmail);
+    await companheiroDocRef.updateData({'color': color});
+  }
+
+//UPDATE EYES
+  updateCompanheiroEyes(String userEmail, String eyes) async {
+    CollectionReference companheiroRef =
+        Firestore.instance.collection('companheiro');
+    DocumentReference companheiroDocRef = companheiroRef.document(userEmail);
+    await companheiroDocRef.updateData({'eyes': eyes});
+  }
+
+//UPDATE HEAD TOP
+  updateCompanheiroHead(String userEmail, String headTop) async {
+    CollectionReference companheiroRef =
+        Firestore.instance.collection('companheiro');
+    DocumentReference companheiroDocRef = companheiroRef.document(userEmail);
+    await companheiroDocRef.updateData({'head_top': headTop});
+  }
+
+//UPDATE SHAPE
+  updateCompanheiroShape(String userEmail, String shape) async {
+    CollectionReference companheiroRef =
+        Firestore.instance.collection('companheiro');
+    DocumentReference companheiroDocRef = companheiroRef.document(userEmail);
+    await companheiroDocRef.updateData({'shape': shape});
+  }
+
