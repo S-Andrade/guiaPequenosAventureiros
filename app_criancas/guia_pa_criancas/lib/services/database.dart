bc22b1dbeb606e5c73228cd81b7abcff722baaae
Ana Vieira <anarafaela98@ua.pt>
2020-06-21 02:56:18 +0100

    dados sociedomograficos a funcionar!

--- app_criancas/guia_pa_criancas/lib/services/database.dart
+++ app_criancas/guia_pa_criancas/lib/services/database.dart
@@ -165,7 +165,7 @@ class DatabaseService {
 }
 
 //info Aluno
-void updateUserData(
+updateUserData(
     String id,
     String idade,
     String genero,
@@ -186,9 +186,10 @@ void updateUserData(
     DateTime idadePai,
     String nacionalidadePai,
     String profissaoPai,
-    String habilitacoesPai) {
+    String habilitacoesPai) async {
+  print('aquiiiiiiiiiiiiiiiiii');
   CollectionReference alunoCollection = Firestore.instance.collection('aluno');
-  alunoCollection.document(id).updateData({
+  await alunoCollection.document(id).updateData({
     'idadeAluno': idade,
     'generoAluno': genero,
     'dataNascimentoAluno': dateTime,
